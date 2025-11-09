7c434b8bfb334d25ed27fb11ac231a1e83d8ad8e
Luke Monroe <monroe.luke36@gmail.com>
2025-11-09 09:54:23 -0500

    Update candy.js

--- candy.js
+++ candy.js
@@ -64,14 +64,14 @@ let chocs = [
         description: "Dark chocolate with brittle toffee candy",
         calories: 90,
         ingredients: "Cocoa butter, Milk, Cream, toffee",
-        img: "assets/toffee_crunch.jpg",
+        img: "../assets/toffee_crunch.jpg",
     },
     {
         chocName: "Plain Supreme",
         description: "Dark chocolate with slivers of milk chocolate",
         calories: 90,
         ingredients: "Cocoa butter, Milk, Cream",
-        img: "assets/plain_hybrid.jpg",
+        img: "./assets/plain_hybrid.jpg",
     },
 ];
 
